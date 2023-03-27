7bbe0494ec731d14a28cede9444fad52b1d55cdc
TonyCR2298 <arcesolisaanthhony@gmail.com>
2023-03-26 19:17:33 -0600

    cambios generales

--- js/app.js
+++ js/app.js
@@ -1,22 +1,6 @@
 
 console.log("hola funciona")
 
-/* ESTILOS BOTON DE WSP */
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  //código a ejecutar cuando el DOM está listo para recibir acciones
-
-    const btnwsp = document.querySelector(".btnwsp");
-
-    window.addEventListener("scroll", () => {
-    if (window.pageYOffset > 100) {
-      btnwsp.classList.add("active");
-    } else {
-      btnwsp.classList.remove("active");
-    }
-    })
-});
-
 /* boton to top */
 document.addEventListener("DOMContentLoaded", function(event) {
   //código a ejecutar cuando el DOM está listo para recibir acciones
@@ -31,7 +15,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
     })
 });
-
-/* pruebas */
 
 
